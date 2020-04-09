1de01891e79fd13a2910a3ec261e17008425f3e2
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-04-09 13:10:51 +0200

    Added Group model

--- Data/Group.cs
+++ Data/Group.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebTeam6.Data
 {
-    public class User
+    public class Group
     {
         [Key]
         public Guid Id { get; set; }
 
         [Required]
-        public string Username { get; set; }
-
-        [Required]
-        public string Email { get; set; }
-
-        [Required]
-        public string Password { get; set; }
+        public string Name { get; set; }
 
         public virtual ICollection<MemberGroup> MemberGroups { get; set; }
 
--- Data/MainContext.cs
+++ Data/MainContext.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Cosmos;
 using Microsoft.EntityFrameworkCore;
+using WebTeam6.Data;
 
 namespace WebTeam6.Data
 {
@@ -16,6 +17,7 @@ namespace WebTeam6.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -23,6 +25,7 @@ namespace WebTeam6.Data
                            "https://localhost:8081",
                            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
                            databaseName: "RemoteTool");
+            optionsBuilder.UseLazyLoadingProxies();
             optionsBuilder.EnableSensitiveDataLogging();
         }
 
@@ -30,6 +33,35 @@ namespace WebTeam6.Data
         {
             modelBuilder.HasDefaultContainer("Tool");
             modelBuilder.Entity<User>().ToContainer("Users");
+            modelBuilder.Entity<Group>().ToContainer("Groups");
+            modelBuilder.Entity<MemberGroup>().ToContainer("MemberGroups");
+            modelBuilder.Entity<OwnerGroup>().ToContainer("OwnerGroups");
+
+
+            modelBuilder.Entity<MemberGroup>().HasKey(mg => new { mg.MemberId, mg.GroupId });
+            modelBuilder.Entity<OwnerGroup>().HasKey(og => new { og.OwnerId, og.GroupId });
+
+
+            modelBuilder.Entity<MemberGroup>().HasOne<User>().WithMany(u => u.MemberGroups).HasForeignKey(mg => mg.MemberId);
+            modelBuilder.Entity<MemberGroup>().HasOne<Group>().WithMany(g => g.MemberGroups).HasForeignKey(mg => mg.GroupId);
+
+            modelBuilder.Entity<OwnerGroup>().HasOne<User>().WithMany(u => u.OwnerGroups).HasForeignKey(og => og.OwnerId);
+            modelBuilder.Entity<OwnerGroup>().HasOne<Group>().WithMany(g => g.OwnerGroups).HasForeignKey(ug => ug.GroupId);
         }
     }
+}
+
+public class MemberGroup
+{
+    public Guid MemberId { get; set; }
+    public virtual User Member { get; set; }
+    public Guid GroupId { get; set; }
+    public virtual Group Group { get; set; }
+}
+public class OwnerGroup
+{
+    public Guid OwnerId { get; set; }
+    public virtual User Owner { get; set; }
+    public Guid GroupId { get; set; }
+    public virtual Group Group { get; set; }
 }
