406378367e18a57564a4753a7cf83eabb464a2d7
ahmadgbg <56073768+ahmadgbg@users.noreply.github.com>
2020-04-23 12:33:42 +0200

    [WEB6-33] Added User Cred in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace WebTeam6
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddDbContext<MainContext>(options => options.UseMySql($"Server={Configuration["MySql:Server"]};Database={Configuration["MySql:Database"]};User={Configuration["MySql:User"]};Password={Configuration["MySql:Password"]}"), ServiceLifetime.Transient);
+            services.AddDbContext<MainContext>(options => options.UseMySql($"Server=remotemysql.com;Database=Kar4xdXASC;User=Kar4xdXASC;Password=HA1veNjEML"), ServiceLifetime.Transient);
             services.AddIdentity<User, IdentityRole>(options => {
                 //Add Password and Username requirements here
             })
