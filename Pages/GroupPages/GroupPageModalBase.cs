57c32cbbe442c50a31d55906586a3e1e20805bb0
Johan Cardell <Johan.Cardell@iths.se>
2020-05-14 19:26:22 +0200

    WIP Working on group page modals

--- Pages/GroupPages/GroupPageModalBase.cs
+++ Pages/GroupPages/GroupPageModalBase.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebTeam6.Data;
+using WebTeam6.Services;
 
 namespace WebTeam6.Pages.GroupPages
 {
-    public class GroupPageModalBase
+    public class GroupPageModalBase: ComponentBase
     {
+        [Inject]
+        public IJSRuntime jSRuntime { get; set; }
+        [Inject]
+        public IGroupService GroupService { get; set; }
+        [Inject]
+        public IUserService UserService { get; set; }
+        [Parameter]
+        public Group GroupObject { get; set; }
+        [Parameter]
+        public Action DataChanged { get; set; }
+        [Parameter]
+        public List<User> Users { get; set; } = new List<User>();
+
+        protected IEnumerable<string> selectedUsers = new string[] { "", "" };
+        protected string value = string.Empty;
+
+        protected async Task CloseModal(string modalId)
+        {
+            await jSRuntime.InvokeAsync<object>("CloseModal", modalId);
+        }
+        protected async Task AddSelectedUsers()
+        {
+            await GroupService.AddMembers(selectedUsers, GroupObject);
+            await CloseModal("addMemberModal");
+            DataChanged?.Invoke();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Users = await UserService.Get();
+        }
+
+        protected void Change(object value, string name)
+        {
+            var str = value is IEnumerable<object> ? string.Join(", ", (IEnumerable<object>)value) : value;
+            StateHasChanged();
+        }
     }
 }
